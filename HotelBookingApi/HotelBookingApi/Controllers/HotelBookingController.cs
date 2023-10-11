a075551e7f943fc9be84a380dc709467671f78dd
quibong <raczka.hugo@gmail.com>
2023-10-11 09:55:48 +0200

    added HotelBookingApi

---
+++ HotelBookingApi/HotelBookingApi/Controllers/HotelBookingController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HotelBookingApi.Models;
+using HotelBookingApi.Data;
+
+namespace HotelBookingApi.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelBookingController : ControllerBase {
+        private readonly ApiContext _context;
+        public HotelBookingController(ApiContext context) {
+            _context = context; 
+        }
+
+        //Create/Edit
+        [HttpPost]
+        public JsonResult CreateEdit (HotelBooking booking) {
+            if(booking.Id == 0) {
+                _context.Bookings.Add(booking);
+            }
+            else {
+                var bookingInDb = _context.Bookings.Find(booking.Id);
+                if(bookingInDb == null) return new JsonResult(NotFound());
+                bookingInDb = booking;
+            }
+            _context.SaveChanges();
+            return new JsonResult(Ok(booking));
+        }
+
+        //
+    }
+}
---
+++ HotelBookingApi/HotelBookingApi/Data/ApiContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HotelBookingApi.Models;
+
+namespace HotelBookingApi.Data {
+    public class ApiContext : DbContext {
+        public DbSet<HotelBooking> Bookings{ get; set; }
+        //Klare Erklärung, warum ich den Base Konstruktor brauche
+        public ApiContext(DbContextOptions<ApiContext> options ) : base(options) {
+                
+        }
+    }
+}
